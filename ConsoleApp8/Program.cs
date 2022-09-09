2bde1b03d89c34afe89d26f7bc6ef769920fe773
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-09 14:21:47 +0530

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedExample
+{
+
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 5; j >= i; j--)
+                {
+                    Console.Write(j +"");     
+                }
+                Console.WriteLine();
+            }
+        }
+        
+       
+       
+
+    }
+}
+
